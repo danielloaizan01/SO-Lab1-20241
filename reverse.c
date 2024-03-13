7f8c8fb090e0fc3f153af0a0f885ad8be0b963a0
Anderson Saldarriaga <anderson.saldarriaga@udea.edu.co>
2024-03-12 22:54:40 -0500

    se comparan los archivos

--- reverse.c
+++ reverse.c
@@ -20,3 +20,24 @@ void openFile(char *file_name, FILE **file) {
     }
 }
 
+void checkFile(char *file1, char *file2) {
+    struct stat file1_stat, file2_stat;
+
+    // Obtener información del primer archivo
+    if (stat(file1, &file1_stat) == -1) {
+        perror("Error obteniendo información del primer archivo");
+        exit(EXIT_FAILURE);
+    }
+
+    // Obtener información del segundo archivo
+    if (stat(file2, &file2_stat) == -1) {
+        perror("Error obteniendo información del segundo archivo");
+        exit(EXIT_FAILURE);
+    }
+
+    // Comparar
+    if (file1_stat.st_dev == file2_stat.st_dev && file1_stat.st_ino == file2_stat.st_ino) {
+        fprintf(stderr, "Los archivos deben ser diferentes\n");
+        exit(EXIT_FAILURE);
+    } 
+}
